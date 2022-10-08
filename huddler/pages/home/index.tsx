172a8227fc7ab699d53ac8c9bdda310ad656000c
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-08 18:02:37 +0200

    feat: implemeneted map and started doing infowindow/markers

--- huddler/pages/home/index.tsx
+++ huddler/pages/home/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Map from "../../src/components/Home-components/Map";
 
 function index() {
-  return <div>index</div>;
+  return (
+    <div>
+      <h1>HOME</h1>
+      <Map />
+    </div>
+  );
 }
 
 export default index;
--- huddler/src/components/Home-components/Map.tsx
+++ huddler/src/components/Home-components/Map.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+export default function Map() {
+  const [showHuddle, setShowHuddle] = useState(true);
+  const containerStyle = {
+    width: "80vw",
+    height: "80vw",
+  };
+  const center = {
+    lat: 41.39,
+    lng: 2.154,
+  };
+  const apiKey = process.env.GOOGLE_API_KEY;
+  const [map, setMap] = useState({});
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyCp_tB1ltdO-fu83Ab_xL6Lp_lK-yRunPc",
+    version: "weekly",
+  });
 
-function Map() {
-  return <div>Map</div>;
+  return isLoaded ? (
+    <GoogleMap
+      // mapTypeId="f0f3505446fe06f1"
+      zoom={12}
+      mapContainerStyle={containerStyle}
+      center={center}
+      onLoad={(map) => setMap(map)}
+      onUnmount={(map) => setMap({})}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      {showHuddle ? (
+        <Marker
+          title="coolio"
+          position={{ lat: 41.39, lng: 2.154 }}
+          onClick={() => setShowHuddle(false)}
+          animation={google.maps.Animation.DROP}
+        />
+      ) : (
+        <InfoWindow
+          onCloseClick={() => setShowHuddle(true)}
+          position={{
+            lat: 41.39,
+            lng: 2.154,
+          }}
+        >
+          <div className="bg-orange-300 ">
+            <h1>InfoWindow</h1>
+          </div>
+        </InfoWindow>
+      )}
+      {/* {markers ? (
+        <Marker
+          position={markers.position}
+          animation={google.maps.Animation.DROP}
+        />
+      ) : (
+        <></>
+      )} */}
+
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
 }
-
-export default Map;
