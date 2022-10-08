9941244d2c06f89cf08e12840dd32974e0d0b244
ov <virtaoskari@gmail.com>
2022-10-08 10:20:33 +0200

    page layout

--- huddler/pages/index.tsx
+++ huddler/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
+const LandingPage: NextPage = () => {
   const env = process.env.TEST;
   console.log(env);
 
@@ -11,4 +11,4 @@ const Home: NextPage = () => {
   );
 };
 
-export default Home;
+export default LandingPage;
