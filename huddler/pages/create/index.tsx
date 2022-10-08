35691d5ca341e9b67a621b87557ea5a3425a6ae1
ov <virtaoskari@gmail.com>
2022-10-08 15:48:06 +0200

    feat(huddles): huddles

--- huddler/pages/create/index.tsx
+++ huddler/pages/create/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Huddles from "../../src/components/Home-components/Huddles";
 import Welcome from "../../src/components/Welcome";
 
 function index() {
   return (
     <div>
-      <Welcome />
+      {/* <Welcome /> */}
+      <Huddles></Huddles>
     </div>
   );
 }
