d07dcb5c4749874aad0ebaf98524a92c31ef073a
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-08 10:43:37 +0200

    fix: fix

---
+++ huddler/src/components/Home-components/HuddleCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HuddleCard() {
+  return <div>HuddleCard</div>;
+}
+
+export default HuddleCard;
---
+++ huddler/src/components/Home-components/Huddles.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Huddles() {
+  return <div>Huddles</div>;
+}
+
+export default Huddles;
