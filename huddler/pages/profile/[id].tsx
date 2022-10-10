3677cfe9439eaa18756a7f490338c1f78b85b0ed
ov <virtaoskari@gmail.com>
2022-10-10 11:25:14 +0200

    fix(merge): merge

--- huddler/pages/profile/[id].tsx
+++ huddler/pages/profile/[id].tsx
@@ -15,7 +15,7 @@ function Profile() {
   };
 
   const tags = ["fishing", "kebab", "snooker", "JavaScript"]
-  
+
   const router = useRouter();
   const [ownedHuddles, setOwnedHuddles] = useState(null);
 
@@ -23,17 +23,35 @@ function Profile() {
 
     <main className='grid grid-cols-4 h-full py-8'>
       <div className='flex flex-col h-screen items-center border-x-[0.2px] border-gray-400'>
-        
 
-      {/* // Avatar  */}
-        <div className='w-full h-[45vh] relative grid'>
+
+        {/* // Avatar  */}
+        <div className='w-full h-full relative grid'>
           <Image
             src={user.avatar!}
             className='rounded-full p-12'
-            alt='avatar'           
+            alt='avatar'
+            
           />
+          <h1 className='text-[2rem] justify-self-center'>Welcome back, {user.name}</h1>
 
-        <h1 className='text-[2rem] justify-self-center'>Welcome back, {user.name}</h1>
+          <div className='grid grid-cols-3 px-24 p-4'>
+            <div className=' border border-black flex flex-col justify-center items-center p-4'>
+              <h1>Huddles</h1>
+              <p>5</p>
+            </div>
+
+            <div className=' border border-black flex flex-col justify-center items-center p-4'>
+              <h1>Something</h1>
+              <p>6</p>
+            </div>
+
+            <div className=' border border-black flex flex-col justify-center items-center p-4'>
+              <h1>Something else</h1>
+              <p>234</p>
+            </div>
+          </div>
+        
         </div>
 
         <div className=' h-56 w-full p-6 px-12 text-lg'>
@@ -60,19 +78,18 @@ function Profile() {
 
       <div className='h-full w-full bg-yellow-200 col-span-3'>
 
-          <h1 className='py-8 p-4 text-3xl'>Interests:</h1>
+        <h1 className='py-8 p-4 text-3xl'>Interests:</h1>
         <div className='flex flex-wrap bg-white gap-4 p-4 border'>
           {tags.map((tag) => (
-            <h1 className='text-xl bg-blue-600 p-2 px-4 rounded text-white'>{tag}</h1>
-          ))}          
+            <h1 className='text-xl bg-blue-600 py-2 px-4 rounded text-white hover:scale-150 hover:mx-4 cursor-pointer'>{tag}</h1>
+          ))}
         </div>
 
         <div>
           <Huddles />
-
         </div>
-        
-          
+
+
       </div>
     </main>
   );
