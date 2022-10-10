a58cbdbb243ba05151023b9281eee793fbd5d4df
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-10 13:18:30 +0200

    feat: map display almost finished

--- huddler/src/components/Home-components/Map.tsx
+++ huddler/src/components/Home-components/Map.tsx
@@ -55,6 +55,7 @@ export default function Map() {
     width: "80vw",
     height: "47vw",
   });
+  // later change center to user location
   const center = {
     lat: 41.39,
     lng: 2.154,
@@ -63,24 +64,14 @@ export default function Map() {
   const [map, setMap] = useState({});
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
-    googleMapsApiKey: "AIzaSyCkRgRrP2nqEQXJxUqBqN9V071kPOn7HoQ",
+    googleMapsApiKey: "AIzaSyAJpYEv9x1Nh8h89d6pysqGfpgfs1C6eGE",
     version: "weekly",
   });
 
   return isLoaded ? (
     <div className="mt-16 mr-6 ">
       <button
-        onClick={() =>
-          setContainerSize({
-            width: "80vw",
-            height: "47vw",
-          })
-        }
-      >
-        +
-      </button>
-      <button
-        className="ml-2"
+        className="ml-2 float-right"
         onClick={() =>
           setContainerSize({
             width: "40vw",
@@ -90,83 +81,96 @@ export default function Map() {
       >
         -
       </button>
-      <GoogleMap
-        zoom={12}
-        mapContainerStyle={containerSize}
-        center={center}
-        onLoad={(map) => setMap(map)}
-        onUnmount={() => setMap({})}
-      >
-        {/* Child components, such as markers, info windows, etc. */}
-        {MOCKDATA ? (
-          MOCKDATA.map((huddle) => {
-            return (
-              <Marker
-                key={huddle.lat}
-                position={{ lat: huddle.lat, lng: huddle.lng }}
-                animation={google.maps.Animation.DROP}
-                onClick={() => setShowHuddle(huddle)}
-              />
-            );
+      <button
+        className=" float-right"
+        onClick={() =>
+          setContainerSize({
+            width: "80vw",
+            height: "47vw",
           })
-        ) : (
+        }
+      >
+        +
+      </button>
+      <div className="shadow-xl">
+        <GoogleMap
+          zoom={12}
+          mapContainerStyle={containerSize}
+          center={center}
+          onLoad={(map) => setMap(map)}
+          onUnmount={() => setMap({})}
+        >
+          {/* Child components, such as markers, info windows, etc. */}
+          {MOCKDATA ? (
+            MOCKDATA.map((huddle) => {
+              return (
+                <Marker
+                  key={huddle.lat}
+                  position={{ lat: huddle.lat, lng: huddle.lng }}
+                  animation={google.maps.Animation.DROP}
+                  onClick={() => setShowHuddle(huddle)}
+                />
+              );
+            })
+          ) : (
+            <></>
+          )}
+          {showHuddle.name ? (
+            <InfoWindowF
+              position={{ lat: showHuddle.lat, lng: showHuddle.lng }}
+              onCloseClick={() =>
+                setShowHuddle({
+                  name: "",
+                  details: "somedetails",
+                  attendants: 532,
+                  lat: 41.3,
+                  lng: 2.154,
+                  checkedIn: false,
+                })
+              }
+            >
+              <div className="animation-fadein">
+                <h1 className="font-bold text-orange-600 mb-1">
+                  {showHuddle.name}
+                </h1>
+                <Image
+                  alt="img"
+                  src={placeholder}
+                  height={200}
+                  width={200}
+                  className="rounded-lg"
+                />
+                <h2 className="mt-1">attendants: {showHuddle.attendants}</h2>
+                <h3 className="h-12 w-48 overflow-auto mt-3">
+                  {showHuddle.details}
+                </h3>
+                {showHuddle.checkedIn ? (
+                  <button
+                    className="float-right flex mt-3 italic font-medium bg-slate-300 p-1 rounded-md w-[4.5rem]"
+                    onClick={() =>
+                      setShowHuddle({ ...showHuddle, checkedIn: false })
+                    }
+                  >
+                    Check out
+                  </button>
+                ) : (
+                  <button
+                    className="float-right flex mt-3 italic font-medium bg-orange-300 p-1 rounded-md w-[4.5rem]"
+                    onClick={() =>
+                      setShowHuddle({ ...showHuddle, checkedIn: true })
+                    }
+                  >
+                    Check in
+                  </button>
+                )}
+              </div>
+            </InfoWindowF>
+          ) : (
+            <></>
+          )}
           <></>
-        )}
-        {showHuddle.name ? (
-          <InfoWindowF
-            position={{ lat: showHuddle.lat, lng: showHuddle.lng }}
-            onCloseClick={() =>
-              setShowHuddle({
-                name: "",
-                details: "somedetails",
-                attendants: 532,
-                lat: 41.3,
-                lng: 2.154,
-                checkedIn: false,
-              })
-            }
-          >
-            <div className="animation-fadein">
-              <h1 className="font-bold text-orange-600 mb-1">
-                {showHuddle.name}
-              </h1>
-              <Image
-                alt="img"
-                src={placeholder}
-                height={200}
-                width={200}
-                className="rounded-lg"
-              />
-              <h2 className="mt-1">attendants: {showHuddle.attendants}</h2>
-              <h3 className="h-12 w-48 overflow-auto mt-3">
-                {showHuddle.details}
-              </h3>
-              {showHuddle.checkedIn ? (
-                <button
-                  className="float-right flex mt-3 italic font-medium bg-slate-300 p-1 rounded-md w-[4.5rem]"
-                  onClick={() =>
-                    setShowHuddle({ ...showHuddle, checkedIn: false })
-                  }
-                >
-                  Check out
-                </button>
-              ) : (
-                <button
-                  className="float-right flex mt-3 italic font-medium bg-orange-300 p-1 rounded-md w-[4.5rem]"
-                  onClick={() =>
-                    setShowHuddle({ ...showHuddle, checkedIn: true })
-                  }
-                >
-                  Check in
-                </button>
-              )}
-            </div>
-          </InfoWindowF>
-        ) : (
-          <></>
-        )}
-        <></>
-      </GoogleMap>
+        </GoogleMap>
+      </div>
     </div>
   ) : (
     <></>
