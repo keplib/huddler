1c7a86a71f2c15f1f6547448d87032cbbf1f2015
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-11 09:42:28 +0200

    feat: added autocomplete component

--- huddler/src/components/Home-components/Map.tsx
+++ huddler/src/components/Home-components/Map.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import placeholder from "../../../public/placeholder.jpg";
 import Image from "next/future/image";
 import {
   GoogleMap,
@@ -7,11 +6,8 @@ import {
   Marker,
   InfoWindowF,
 } from "@react-google-maps/api";
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
-import useOnclickOutside from "react-cool-onclickoutside";
+import PlacesAutocomplete from "./PlacesAutocomplete";
+
 export default function Map({ huddles }) {
   const [showHuddle, setShowHuddle] = useState({
     name: "",
@@ -31,7 +27,6 @@ export default function Map({ huddles }) {
     lng: 2.154,
   });
   const [map, setMap] = useState({});
-  const [autocomplete, setAutocomplete] = useState(null);
   const [selected, setSelected] = useState(null);
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
@@ -39,78 +34,6 @@ export default function Map({ huddles }) {
     version: "weekly",
     libraries: ["places"],
   });
-  const PlacesAutocomplete = () => {
-    const {
-      ready,
-      value,
-      suggestions: { status, data },
-      setValue,
-      clearSuggestions,
-    } = usePlacesAutocomplete({
-      requestOptions: {
-        /* Define search scope here */
-      },
-      debounce: 300,
-    });
-    const ref = useOnclickOutside(() => {
-      // When user clicks outside of the component, we can dismiss
-      // the searched suggestions by calling this method
-      clearSuggestions();
-    });
-
-    const handleInput = (e) => {
-      // Update the keyword of the input element
-      setValue(e.target.value);
-    };
-
-    const handleSelect =
-      ({ description }) =>
-      () => {
-        // When user selects a place, we can replace the keyword without request data from API
-        // by setting the second parameter to "false"
-        setValue(description, false);
-        clearSuggestions();
-
-        // Get latitude and longitude via utility functions
-        getGeocode({ address: description }).then((results) => {
-          const { lat, lng } = getLatLng(results[0]);
-          setCenter({ lat, lng });
-          setSelected(true);
-        });
-      };
-
-    const renderSuggestions = () =>
-      data.map((suggestion) => {
-        const {
-          place_id,
-          structured_formatting: { main_text, secondary_text },
-        } = suggestion;
-
-        return (
-          <li
-            key={place_id}
-            onClick={handleSelect(suggestion)}
-            className="bg-white py-1 px-2 border-black border-solid w-[100%] cursor-pointer"
-          >
-            <strong>{main_text}</strong> <small>{secondary_text}</small>
-          </li>
-        );
-      });
-
-    return (
-      <div ref={ref}>
-        <input
-          className="py-1 outline-none placeholder: pl-[0.5rem] w-[100%]"
-          value={value}
-          onChange={handleInput}
-          disabled={!ready}
-          placeholder="Look for a Place . . ."
-        />
-        {/* We can use the "status" to decide whether we should display the dropdown or not */}
-        {status === "OK" && <ul>{renderSuggestions()}</ul>}
-      </div>
-    );
-  };
   return isLoaded ? (
     <div className="mt-16 mr-6 relative">
       {containerSize.width == "80vw" ? (
@@ -140,7 +63,7 @@ export default function Map({ huddles }) {
       )}
       <div className="shadow-xl">
         <div className="absolute z-10 mt-40 ml-4 shadow-md rounded-md w-50">
-          <PlacesAutocomplete />
+          <PlacesAutocomplete hook={setCenter} setSelected={setSelected} />
         </div>
         <GoogleMap
           zoom={12}
@@ -151,13 +74,17 @@ export default function Map({ huddles }) {
         >
           {/* Child components, such as markers, info windows, etc. */}
           {selected && (
-            <Marker position={center} animation={google.maps.Animation.DROP} />
+            <Marker
+              position={center}
+              animation={google.maps.Animation.DROP}
+              draggable={true}
+            />
           )}
           {huddles ? (
             huddles.map((huddle) => {
               return (
                 <Marker
-                  // key={huddle.id + ""}
+                  key={huddle.id + ""}
                   position={{
                     lat: Number(huddle.latitude),
                     lng: Number(huddle.longitude),
---
+++ huddler/src/components/Home-components/PlacesAutocomplete.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import useOnclickOutside from "react-cool-onclickoutside";
+
+const PlacesAutocomplete = ({ hook, setSelected }) => {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      /* Define search scope here */
+    },
+    debounce: 300,
+  });
+  const ref = useOnclickOutside(() => {
+    // When user clicks outside of the component, we can dismiss
+    // the searched suggestions by calling this method
+    clearSuggestions();
+  });
+
+  const handleInput = (e) => {
+    // Update the keyword of the input element
+    setValue(e.target.value);
+  };
+
+  const handleSelect =
+    ({ description }) =>
+    () => {
+      // When user selects a place, we can replace the keyword without request data from API
+      // by setting the second parameter to "false"
+      setValue(description, false);
+      clearSuggestions();
+
+      // Get latitude and longitude via utility functions
+      getGeocode({ address: description }).then((results) => {
+        const { lat, lng } = getLatLng(results[0]);
+        hook({ lat, lng });
+        setSelected(true);
+      });
+    };
+
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      const {
+        place_id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion;
+
+      return (
+        <li
+          key={place_id}
+          onClick={handleSelect(suggestion)}
+          className="bg-white py-1 px-2 border-black border-solid w-[100%] cursor-pointer"
+        >
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </li>
+      );
+    });
+
+  return (
+    <div ref={ref}>
+      <input
+        className="py-1 outline-none placeholder: pl-[0.5rem] w-[100%]"
+        value={value}
+        onChange={handleInput}
+        disabled={!ready}
+        placeholder="Look for a Place . . ."
+      />
+      {/* We can use the "status" to decide whether we should display the dropdown or not */}
+      {status === "OK" && <ul>{renderSuggestions()}</ul>}
+    </div>
+  );
+};
+
+export default PlacesAutocomplete;
