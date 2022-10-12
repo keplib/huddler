17f8037e0fa8a2d26240b9ec02966f266a1575e3
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-12 14:56:44 +0200

    fix: merge fix

--- huddler/pages/home/index.tsx
+++ huddler/pages/home/index.tsx
@@ -3,21 +3,25 @@ import Huddles from "../../src/components/Home-components/Huddles";
 import Map from "../../src/components/Home-components/Map";
 import Search from "../../src/components/Home-components/Search";
 import { getUserCategories } from "../../src/utils/APIServices/userServices";
-import { getHuddlesInCategory } from "../../src/utils/APIServices/categoryServices";
+import {
+  getHuddlesInCategory,
+  getAllCategories,
+} from "../../src/utils/APIServices/categoryServices";
 import { useState } from "react";
 import { User } from "../../src/types";
+import { getAllHuddles } from "../../src/utils/APIServices/huddleServices";
 
 function index() {
   // get current User from auth
 
-  // const [currentUser, setCurrentUser] = useState() //Pass it the user given by Auth
+  const [currentUser, setCurrentUser] = useState(); //Pass it the user given by Auth
 
-  // const { data: huddles, error: huddleError } = getAllHuddles();
-  // const { data: categories, error: catError } = getAllCategories()
+  const { data: huddles, error: huddleError } = getAllHuddles();
+  const { data: categories, error: catError } = getAllCategories();
 
-  const { data: userCategories, error: error } = getUserCategories();
-  
-  const { data: huddlesInCategory, error: error } = getHuddlesInCategory();
+  // const { data: userCategories, error: error } = getUserCategories();
+
+  // const { data: huddlesInCategory, error: error } = getHuddlesInCategory();
 
   if (userCategoriesError || error) return <div>failed to load</div>;
   if (!huddles || !categories) return <div>loading...</div>;
