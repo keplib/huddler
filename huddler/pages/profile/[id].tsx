2c17fc8696bd8ca510bd39880f3d6b5462d1478f
ov <virtaoskari@gmail.com>
2022-10-12 23:49:03 +0200

    feat(util): get recommended huddles by user

--- huddler/pages/profile/[id].tsx
+++ huddler/pages/profile/[id].tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { User } from '../../src/types';
-import { useRouter } from 'next/router';
+import router, { useRouter } from 'next/router';
 import { triggerAsyncId } from 'async_hooks';
 import Huddles from '../../src/components/Home-components/Huddles';
 import Avatar from '../../src/components/Profile components/Avatar';
@@ -9,7 +9,7 @@ import avatar from '../../public/placeholder.jpg';
 import UserInfo from '../../src/components/Profile components/UserInfo';
 import useSWR from 'swr';
 import { fetcher } from '../../src/utils/APIServices/fetcher';
-import { getAllHuddles } from '../../src/utils/APIServices/huddleServices';
+import { getAllHuddles, recommendedForUser } from '../../src/utils/APIServices/huddleServices';
 import HuddleCarousel from '../../src/components/Profile components/HuddleCarousel';
 
 function Profile() {
@@ -19,7 +19,7 @@ function Profile() {
   const { data: userCreatedHuddles, error: userHuddleError } = useSWR(`https://u4pwei0jaf.execute-api.eu-west-3.amazonaws.com/test/huddles_user_created?user-id=${67}`, fetcher)
   const { data: huddles, error: huddleError } = getAllHuddles();
 
-  if (huddleError || tagsError || userHuddleError ) return <div>failed to load</div>;
+  if (huddleError || tagsError || userHuddleError) return <div>failed to load</div>;
   if (!huddles || !tags || !userCreatedHuddles) return <div>loading...</div>;
 
   const user: User = {
@@ -29,19 +29,15 @@ function Profile() {
     createdOn: 0,
   };
 
-  
-  // const recommendedForUser = async () => {
-  //   let recommendation = [];
-  //   // const data = await fetcher(`https://u4pwei0jaf.execute-api.eu-west-3.amazonaws.com/test/users_categories?user-id=${67}`)
-  //     await tags.forEach(async element => {
-  //     let temp =  await fetcher(`https://u4pwei0jaf.execute-api.eu-west-3.amazonaws.com/test/gethuddles_bycategory?category-id=${element.id}`);            
-  //     await recommendation.push(temp);
-  //     console.log(temp)
-  //   });   
-  // }
-  // recommendedForUser();
-  // console.log(userCreatedHuddles);
-  // console.log(tags)
+  useEffect(() => {
+    wtf();
+  }, [])
+
+  const wtf = async () => {
+    console.log(await recommendedForUser(68));
+  }
+  console.log(recommendedForUser(68));
+
 
   return (
     <main className='grid grid-cols-4 h-full py-8'>
@@ -65,7 +61,7 @@ function Profile() {
 
         <h1 className='py-8 p-4 text-3xl'>Interests:</h1>
         <div className='flex flex-wrap bg-white gap-4 p-4 border'>
-          {tags.map((tag,i) => (
+          {tags.map((tag, i) => (
             <h1 className='text-xl bg-blue-600 py-2 px-4 rounded text-white hover:scale-150 hover:mx-4 cursor-pointer' key={i}>{tag.name}</h1>
           ))}
         </div>
@@ -76,9 +72,9 @@ function Profile() {
             <div className='bg-red-500 gap-4 grid grid-cols-2 flex-grow-1 flex-shrink-0 border-black border relative' key={hud.id}>
 
               <HuddleCarousel hud={hud} />
-              
+
             </div>
-          ))}         
+          ))}
         </div>
 
         <h1 className='py-8'>My huddles:</h1>
