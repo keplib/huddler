d05828fa2edb4e6cd98e44c3701cbfb5a6ac6c45
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-13 16:21:47 +0200

    fix: merge fix

--- huddler/src/components/Navbar-components/Dropdown.tsx
+++ huddler/src/components/Navbar-components/Dropdown.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState, useRef, useEffect, EventHandler } from 'react';
 import { CgProfile } from 'react-icons/cg';
 import { FiSettings } from 'react-icons/fi';
@@ -7,11 +8,21 @@ import { AiOutlineCompass } from 'react-icons/ai';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+=======
+import React, { useState, useRef, useEffect, EventHandler } from "react";
+import { CgProfile } from "react-icons/cg";
+import { FiSettings } from "react-icons/fi";
+import { HiOutlineLogout } from "react-icons/hi";
+import { AiOutlineCompass } from "react-icons/ai";
+
+import Link from "next/link";
+import { useRouter } from "next/router";
+>>>>>>> de4cc17 (fix: fixed bug with dropdown icons)
 const serviceDropdown = [
-  { name: 'Explore', path: '/home', icon: <AiOutlineCompass /> },
-  { name: 'Profile', path: '/profile', icon: <CgProfile /> },
-  { name: 'Settings', path: '/settings', icon: <FiSettings /> },
-  { name: 'Log Out', path: '/', icon: <HiOutlineLogout /> },
+  { name: "Explore", path: "/home", icon: <AiOutlineCompass /> },
+  { name: "Profile", path: "/profile", icon: <CgProfile /> },
+  { name: "Settings", path: "/settings", icon: <FiSettings /> },
+  { name: "Log Out", path: "/", icon: <HiOutlineLogout /> },
 ];
 
 type Props = {
@@ -26,12 +37,12 @@ const Dropdown = ({ setShowDropDown }: Props) => {
     e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
   ) => {
     // some function from authentication logout
-    console.log('hit hereeee');
-    router.replace('/');
+    console.log("hit hereeee");
+    router.replace("/");
   };
 
   useEffect(() => {
-    document.addEventListener('click', handleClickOutsideDropdown, true);
+    document.addEventListener("click", handleClickOutsideDropdown, true);
   }, []);
 
   function handleClickOutsideDropdown(this: HTMLElement) {
@@ -42,18 +53,31 @@ const Dropdown = ({ setShowDropDown }: Props) => {
   return (
     <div
       ref={insideDropDownRef}
+<<<<<<< HEAD
       className='mt-24 w-full'
+=======
+      className="mt-24 w-full"
+>>>>>>> de4cc17 (fix: fixed bug with dropdown icons)
       // onMouseLeave={() => setShowDropDown(false)}
     >
-      <ul className='grid grid-cols-3 gap-4 border-b w-64 absolute bg-palette-dark -right-[50%] place-content-center pt-3'>
+      <ul className="grid grid-cols-3 gap-4 border-b w-64 absolute bg-palette-dark -right-[50%] place-content-center pt-3">
         {serviceDropdown.map((menuItem, i) => {
           return (
+<<<<<<< HEAD
             <Link
               href={menuItem.path}
               key={i}
             >
               <a className='col-span-3 border-b p-5 flex gap-4 text-2xl items-center font-bold'>
                 <p className='text-3xl'>{menuItem.icon}</p>
+=======
+            <Link href={menuItem.path}>
+              <a
+                className="col-span-3 border-b p-5 flex gap-4 text-2xl items-center font-bold"
+                key={i}
+              >
+                <p className="text-3xl">{menuItem.icon}</p>
+>>>>>>> de4cc17 (fix: fixed bug with dropdown icons)
                 <p>{menuItem.name}</p>
               </a>
             </Link>
@@ -65,4 +89,7 @@ const Dropdown = ({ setShowDropDown }: Props) => {
 };
 
 export default Dropdown;
+<<<<<<< HEAD
 
+=======
+>>>>>>> de4cc17 (fix: fixed bug with dropdown icons)
