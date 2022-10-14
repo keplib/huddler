aac62e7e4ce19a37b24557e562e4af2c12814225
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-14 10:14:08 +0200

    fix: small fix

--- huddler/src/components/NewUserForm/SecondLocation.tsx
+++ huddler/src/components/NewUserForm/SecondLocation.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { categoryTags } from '../../categoryTags';
+import React from "react";
+import { categoryTags } from "../../categoryTags";
 
 // Contains a form for the categories
 
@@ -7,14 +7,13 @@ type Props = {
   location: any;
   setLocation: any;
 };
-function Location({ setLocation, location }: Props) 
-// some logic where the user chooses its location and updates with setLocation()
-{
+function Location({ setLocation, location }: Props) {
+  // some logic where the user chooses its location and updates with setLocation()
   return (
-    <div className='flex text-2xl font-bold flex-col py-8 w-3/5'>
+    <div className="flex text-2xl font-bold flex-col py-8 w-3/5">
       <h1>{"What's your location?"}</h1>
 
-      <div className='h-1/2 border border-black'>
+      <div className="h-1/2 border border-black">
         <p> Map here? </p>
       </div>
     </div>
