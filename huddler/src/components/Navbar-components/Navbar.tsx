9acd32a65e8ede9c3ea7d14c722e147761b60fed
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-14 14:34:35 +0200

    feat: mvp presentation pulse added

--- huddler/src/components/Navbar-components/Navbar.tsx
+++ huddler/src/components/Navbar-components/Navbar.tsx
@@ -1,26 +1,33 @@
-import React, { useState } from 'react';
-import Image from 'next/future/image';
-import placeholder from '../../../public/placeholder.jpg';
-import Dropdown from './Dropdown';
-import Link from 'next/link';
+import React, { useState } from "react";
+import Image from "next/future/image";
+import placeholder from "../../../public/placeholder.jpg";
+import Dropdown from "./Dropdown";
+import Link from "next/link";
 
 function Navbar() {
   const [showDropDown, setShowDropDown] = useState(false);
 
   const handleClickOnImg = () => {
-    console.log('click' ,showDropDown)
+    console.log("click", showDropDown);
     setShowDropDown(!showDropDown);
   };
 
   return (
     <div className="h-24 w-full bg-palette-dark text-white flex items-center justify-between fixed top-0 px-12 z-10 border-b border-palette-orange">
-      <Link href={'/home'}><a>Logo here</a></Link>
+      <Link href={"/home"}>
+        <a className="animate-pulse">
+          <b>
+            {" "}
+            <i>Huddler</i>
+          </b>
+        </a>
+      </Link>
       <div className="w-24 h-24 relative">
         <Image
           src={placeholder}
-          alt='placeholder'
+          alt="placeholder"
           fill
-          className=' rounded-full p-1 cursor-pointer'
+          className=" rounded-full p-1 cursor-pointer"
           onClick={() => handleClickOnImg()}
         />
         {showDropDown && <Dropdown setShowDropDown={setShowDropDown} />}
