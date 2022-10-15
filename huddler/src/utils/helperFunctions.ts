58c1505318cd20e5f76da81a4fdab0ec03352811
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-15 16:51:00 +0200

    fix: sorry if i break anything

--- huddler/src/utils/helperFunctions.ts
+++ huddler/src/utils/helperFunctions.ts
@@ -1,7 +1,7 @@
-import dayjs from 'dayjs';
-import { Category } from '../types';
-import { getHuddlesInCategory } from './APIServices/categoryServices';
-import { getUserCategories } from './APIServices/userServices';
+import dayjs from "dayjs";
+import { Category } from "../types";
+import { getHuddlesInCategory } from "./APIServices/categoryServices";
+import { getUserCategories } from "./APIServices/userServices";
 
 // 1. Fetcher
 // 2. Recommended Huddles
@@ -15,7 +15,7 @@ export const fetcher = async (...args: string[]) => {
     const data = await fetch(...args);
     return await data.json();
   } catch (e) {
-    console.log('There has been an error fetching data: ', e);
+    console.log("There has been an error fetching data: ", e);
     return e;
   }
 };
@@ -23,9 +23,9 @@ export const fetcher = async (...args: string[]) => {
 // 2. Recommended Huddles
 
 //For now this functions returns all the huddles that are in user categories. Eventually we should do some kind of sorting or also recommend by location. Now we don't have enough huddles in each categories to test it.
-export const recommendedForUser = async (userId: number) => {
-  const userCategories = await getUserCategories(userId);
-  console.log('userCategories', userCategories);
+export const recommendedForUser = async (aws_id: string) => {
+  const userCategories = await getUserCategories(aws_id);
+  console.log("userCategories", userCategories);
   const promises = userCategories.map((category: Category) =>
     getHuddlesInCategory(category.id as number)
   );
@@ -41,20 +41,20 @@ export const recommendedForUser = async (userId: number) => {
 export const dateFormatter = (date: string) => {
   const toFormat = dayjs(date);
   const dateTime = {
-    day: toFormat.format('DD'),
-    month: toFormat.format('MMMM'),
-    year: toFormat.format('YYYY'),
-    time: toFormat.format('hh:mmA'),
-    monthDayYear: toFormat.format('MMMM DD, YYYY'),
+    day: toFormat.format("DD"),
+    month: toFormat.format("MMMM"),
+    year: toFormat.format("YYYY"),
+    time: toFormat.format("hh:mmA"),
+    monthDayYear: toFormat.format("MMMM DD, YYYY"),
   };
   return dateTime;
 };
 
 export const nowFormatted = () => {
-  return dayjs(Date.now()).format('YYYY-MM-DDTHH:mm');
+  return dayjs(Date.now()).format("YYYY-MM-DDTHH:mm");
 };
 
 // 4. Sort by name
 export const sortByName = (arrOfObj) => {
   return arrOfObj.sort((a, b) => a.name.localeCompare(b.name));
-}
+};
