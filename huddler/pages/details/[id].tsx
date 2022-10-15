d7f0b2671e75fee173a23b7647bac412ebe39e89
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-15 16:33:41 +0200

    fix: updating to use services

--- huddler/pages/details/[id].tsx
+++ huddler/pages/details/[id].tsx
@@ -25,7 +25,7 @@ const Details = () => {
     <div className="flex">
       <div
         id="huddle-details"
-        className="px-8 py-6 bg-white bg-opacity-20 shadow-xl"
+        className="h-screen w-[19rem] px-8 py-6 pb-0 mt-12 bg-white bg-opacity-20 shadow-xl"
       >
         <p className="text-4xl font-extrabold text-palette-orange">
           {huddle.name}
@@ -45,15 +45,31 @@ const Details = () => {
         <p>{huddle.address}</p>
         <p className="mt-4">Created By: {creator?.username}</p>
         <p className="my-2">Who's going:</p>
-        {users ? (
+        {/* {users ? (
           users.map((user: any) => {
             return <p className="cursor-pointer mb-1">{user.username}</p>;
           })
         ) : (
           <></>
-        )}
+        )} */}
       </div>
-      <div id="huddle-chat"></div>
+      <div id="huddle-chat" className="grid grid-cols-1 w-full mb-24">
+        <div className="border border-palette-orange mx-14 my-24 p-4 rounded-2xl bg-white bg-opacity-20 relative">
+          <div>
+            <ul>
+              <li>hello</li>
+              <li>hi</li>
+            </ul>
+          </div>
+          <div>
+            <input
+              type="text"
+              className="absolute inset-x-0 bottom-0 mb-6 rounded-xl h-12 w-full"
+            ></input>
+            <button>Enter</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
